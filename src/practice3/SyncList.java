7883d300ba8a5ea43a50b10b751c0b911e98b727
Georgy <savchukg89@gmail.com>
2024-02-25 17:43:55 +0300

    Практическая работа 3

---
+++ src/practice3/SyncList.java
@@ -0,0 +1,29 @@
+package practice3;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SyncList<T>{
+    private List<T> list;
+
+    public SyncList() {
+        this.list = new ArrayList<>();
+    }
+
+    public synchronized void add(T element) {
+        list.add(element);
+    }
+
+    public synchronized T get(int index) {
+        return list.get(index);
+    }
+
+    public synchronized int size() {
+        return list.size();
+    }
+
+    public synchronized boolean isEmpty() {
+        return list.isEmpty();
+    }
+}
